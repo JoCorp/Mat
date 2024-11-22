fd0bf56bc1768f3b6865f0519b6f6d173c06c16e
JoCorp <gius.oricchio@gmail.com>
2024-11-22 21:32:42 +0100

    code optimized

--- sp_excercise.m
+++ sp_excercise.m
@@ -17,10 +17,12 @@ figure(1);
 subplot(2,1,1)
 plot(time, y(:,1), time, y(:,2), time, y(:,3), time, y(:,4), time, y(:,5), time, y(:,6), time, y(:,7), time, y(:,8), time, y(:,9));
 xlim([0 10])
-ylim([0 1])
+ylim([0 10])
 legend('Sox9', 'mSox9', 'Sox10', 'mSox10', 'Tcf1', 'mTcf1', 'miR155', 'miR335', 'miR338');
 subplot(2,1,2)
 plot(time, y(:,10));
 xlim([0 10])
 ylim([0 10])
 legend('OUT');
+
+% strange behaviour of time (gets lower multiple times)
