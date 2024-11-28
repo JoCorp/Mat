a6547c0f97e208fb1979269d76f0bc9a15a53c35
JoCorp <gius.oricchio@gmail.com>
2024-11-28 19:12:26 +0100

    Some thoughts

--- model1.m
+++ model1.m
@@ -3,6 +3,10 @@ function R1 = model1(t,initCond,p)
 R2 = initCond(2);
 u = initCond(3);
 
+% this is a very stupid way to solve the R1_0 problem. It would be much
+% easier to use an additional value returned by the function R1. However
+% I'm afraid it wouldn't work for the ODE function
+
 if t == 0;
     R1_x = initCond(1);
 else
