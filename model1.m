834c0a7ef07c3cf29b602c52c4b46605ecb5df9e
JoCorp <gius.oricchio@gmail.com>
2024-11-29 09:28:58 +0100

    1. Attempt to solve R1 problem in ODE function
    T is not a suitable index

--- model1.m
+++ model1.m
@@ -1,13 +1,24 @@
 function R1 = model1(t,initCond,p)
 
-R1 = [initCond(1)];
+R1 = [0];
+
+% values of R2 and u assigned directly in function
+
 R2 = 1;
 u = 1;
 
 % work in progress here. I have to convert t to an integer
 
-T = cast(t, int) - 1
+% conversion works but time is not a suitable index (49 elements, but time
+% reaches max. value of 10
 
-R1 = p.ks1 * (u/(1 + (R2/p.K2))) - p.k1 * R1(T)
+% if t < 2
+    X = initCond(1);
+%elseif t >= 2
+%T = cast(t, "int8") - 1;
+%X = R1(T);
+%end
+
+R1 = p.ks1 * (u/(1 + (R2/p.K2))) - p.k1 * X;
 
 end
