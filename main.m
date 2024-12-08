1cac58f20a68b1ed025e2c08ed04a89f2bd46b73
JoCorp <gius.oricchio@gmail.com>
2024-12-08 14:33:29 +0100

    k2>7 fixed by decreasing step size

--- main.m
+++ main.m
@@ -200,6 +200,70 @@ ylim([0 20]);
 legend('R - ode45', 'R - Euler');
 title('Comparison of R: ODE45 vs Euler Method');
 
+%% Task 2.2.
+
+% Time range 
+time = [0 10];
+
+% Initial conditions for R1 and R2
+y0 = [3, 0];  
+k2 = 1:1:10;
+
+for i = 1:length(k2)
+    p.k2 = k2(i);
+
+    % Calculation using ODE function
+
+    [time_odek2{i}, Rk2{i}] = ode45(@(t, y) task2model(t, y, p), time, y0);
+
+    % Extract R1 and R2 from the ODE solution 
+
+    R1_odek2{i} = Rk2{i}(:, 1);
+    R2_odek2{i} = Rk2{i}(:, 2);
+    
+
+    % Calculation using Euler method (task2euler)
+    
+    [R22{i}, timeRangek2{i}] = task2euler(time, 80, y0, p);
+
+    % Extract R2 from Euler method results (the second column)
+    R1_eulerk2{i} = R22{i}(:,1);
+    R2_eulerk2{i} = R22{i}(:,2);
+
+end
+
+% creating a dynamic plot with dynamic labels
+
+figure(4)
+subplot(2,1,1)
+hold on
+for i = 1:length(k2);
+    plot(time_odek2{i}, R2_odek2{i}, 'LineStyle', '-', ...
+        'DisplayName', sprintf('R2, k2 = %d', k2(i)))
+     plot(time_odek2{i}, R1_odek2{i}, 'LineStyle', '--', ...
+         'DisplayName', sprintf('R1, k2 = %d', k2(i)))
+end
+legend('Location', 'eastoutside');
+xlabel('Time');
+ylabel('Gene Expression');
+title('ode45 calculation for different k2')
+subplot(2,1,2)
+hold on
+for i = 1:length(k2);
+    plot(timeRangek2{i}, R2_eulerk2{i},'LineStyle', '-', ...
+        'DisplayName', sprintf('R2, k2 = %d', k2(i)));
+    plot(timeRangek2{i}, R1_eulerk2{i}, 'LineStyle', '--', ...
+         'DisplayName', sprintf('R1, k2 = %d', k2(i)))
+    
+end
+legend('Location', 'eastoutside');
+xlabel('Time');
+ylabel('Gene Expression');
+xlim([0 10]);
+ylim([0 20]);
+title('Task 2.2:Influence of parameter k2 ');
+
+
 %%
 
 
@@ -212,7 +276,7 @@ title('Comparison of R: ODE45 vs Euler Method');
 % Präsentation so strukturieren, dass es am meisten Sinn macht, nicht so,
 % wie die Aufgaben sortiert sind
 
-% Im dePrüfung geht es mehr um die mathematischen Zusammenhänge nicht um
+% In der Prüfung geht es mehr um die mathematischen Zusammenhänge nicht um
 % Code
 
 % Bei Task 3 Euler weglassen
