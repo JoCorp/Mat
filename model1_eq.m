df00e910fc3e246bad3f641f1bf53f367dd7883c
JoCorp <gius.oricchio@gmail.com>
2024-12-08 18:13:21 +0100

    seperate plots for equlibrium added

--- model1_eq.m
+++ model1_eq.m
@@ -1,9 +1,11 @@
-function R1 = model1(t,initCond,p)
+function dR_eq = model1_eq(t,initCond,p)
+
 
 % initial Value of R1 is used directly for initiation instead of zeros
 % function. This way R1(end) can be used in function
 
 R1 = [initCond(1)];
+
 
 % values of R2 and u assigned directly in function. Having them as initCond
 % would increase the array length of the output of the ODE function
@@ -13,7 +15,8 @@ u = 1;
 
 % function to calculate R1
 
-R1 = p.ks1 * (u/(1 + ((R2/p.K2)^p.n))) - p.k1 * R1(end);
+R1_2 = p.k1 * R1(end);
+R1 = p.ks1 * (u/(1 + ((R2/p.K2)^p.n))) - R1_2;
 
-
+dR_eq = [R1; R1_2];
 end
